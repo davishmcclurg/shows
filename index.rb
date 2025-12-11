ac56e6decbe5991776d8030cf0593f4ea4e8118d
David Harsha <davishmcclurg@gmail.com>
2025-12-11 10:12:42 -0800

    Try moving requires into gemfile
    Still seeing openssl errors on github:
    
    ```
    /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/protocol.rb:46:in 'OpenSSL::SSL::SSLSocket#connect_nonblock': SSL_connect returned=1 errno=0 peeraddr=104.18.30.9:443 state=error: certificate verify failed (unable to get certificate CRL) (OpenSSL::SSL::SSLError)
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/protocol.rb:46:in 'Net::Protocol#ssl_socket_connect'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/http.rb:1736:in 'Net::HTTP#connect'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/http.rb:1636:in 'Net::HTTP#do_start'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/http.rb:1625:in 'Net::HTTP#start'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/net/http.rb:1064:in 'Net::HTTP.start'
    	from index.rb:207:in 'block (2 levels) in <main>'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/open-uri.rb:167:in 'OpenURI.open_uri'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/open-uri.rb:805:in 'OpenURI::OpenRead#open'
    	from /Users/runner/hostedtoolcache/Ruby/3.4.7/arm64/lib/ruby/3.4.0/open-uri.rb:25:in 'URI.open'
    	from index.rb:201:in 'block in <main>'
    	from index.rb:62:in 'BasicObject#instance_eval'
    	from index.rb:62:in 'Venue#initialize'
    	from index.rb:200:in 'Venue.new'
    	from index.rb:200:in '<main>'
    ```

--- index.rb
+++ index.rb
@@ -1,17 +1,20 @@
-require 'base64'
-require 'date'
-require 'digest'
-require 'erb'
-require 'json'
-require 'open-uri'
-require 'rss'
-
 require 'bundler/inline'
 
 gemfile do
   source 'https://rubygems.org'
+
+  # https://github.com/ruby/openssl/issues/949
+  gem 'openssl', '~> 3.3.2'
+
+  gem 'base64'
+  gem 'date'
+  gem 'digest'
+  gem 'erb'
+  gem 'json'
+  gem 'open-uri'
+  gem 'rss'
+
   gem 'nokogiri'
-  gem 'openssl', '~> 3.3.2' # https://github.com/ruby/openssl/issues/949
 end
 
 Show = Struct.new(:time, :link, :title, :description, :venue, :keyword_init => true) do
