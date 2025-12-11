44a4392cd806379872b09576b907a48fbdc15fd5
David Harsha <davishmcclurg@gmail.com>
2025-12-11 10:29:06 -0800

    Try separate gemfile
    I can't figure out why github actions is getting the wrong version of
    openssl.

--- index.rb
+++ index.rb
@@ -1,21 +1,5 @@
-require 'bundler/inline'
-
-gemfile do
-  source 'https://rubygems.org'
-
-  # https://github.com/ruby/openssl/issues/949
-  gem 'openssl', '~> 3.3.2'
-
-  gem 'base64'
-  gem 'date'
-  gem 'digest'
-  gem 'erb'
-  gem 'json'
-  gem 'open-uri'
-  gem 'rss'
-
-  gem 'nokogiri'
-end
+require 'bundler'
+Bundler.require
 
 puts "OpenSSL::VERSION: #{OpenSSL::VERSION}"
 
